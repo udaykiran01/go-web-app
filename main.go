a9f9260a1fa11fb7e2fc2729098688daca7547f5
uday akula <udaykiran0117@gmail.com>
2025-06-16 16:30:55 -0400

    Add Go web app with Helm chart

--- main.go
+++ main.go
@@ -1,39 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
-
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
+	fmt.Println("🚀 Server starting at http://localhost:8080")
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("❌ Server failed to start: ", err)
 	}
 }
